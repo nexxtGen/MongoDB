cfc6e5f94c7952ba08a709e3df157fbf9e29c597
Kamil Szczypior <kszczypior1@gmail.com>
2018-11-25 17:02:04 +0100

    Finish initialize mongoose and create instances of users.

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const mongoose = require ('mongoose');
 const Schema = mongoose.Schema; // Pobieram główny konstruktor modelu mongoose
 
 mongoose.Promise = global.Promise;
+
+
 mongoose.connect('mongodb://localhost/nodeappdatabase', {
-    useMongoClinet: true
+    useNewUrlParser: true
 });
+ 
 
 //Tworzę schemat dla aplikacji
 const userSchema = new Schema({
@@ -16,16 +19,14 @@ const userSchema = new Schema({
     updated_at:Date
 });
 
-//Tworzę model na podstawie schematu
-const User = mongoose.model('User', userSchema)
-
 //Tworzę metody dla modelu Mongoose
 userSchema.methods.manify = function(next) {
-    this.name = this.name + '-boy';
+    this.name = this.name + '-men';
 
     return next(null, this.name);
 };
 
+//Metody pre-save
 userSchema.pre('save', function(next){
     //pobranie aktualnego czasu
     const currentDate = new Date();
@@ -41,4 +42,58 @@ userSchema.pre('save', function(next){
     next();
 })
 
+//Tworzę model na podstawie schematu
+const User = mongoose.model('User', userSchema);
 
+// Create instances of user object
+const kamil = new User({
+    name: 'Kamil',
+    username: 'coincidence',
+    password: 'password'
+});
+
+//wywołanie metody
+kamil.manify(function(err, name){
+    if (err) throw err;
+    console.log('Twoje nowe imię to: ' + name);
+});
+
+kamil.save(function(err){
+    if (err) throw err;
+
+    console.log('User zapisany pomyślnie');
+});
+//Tworzę więcej userów
+const alpha = new User({
+    name: 'Alpha',
+    username: 'Alpha_wolf',
+    password: 'password'
+});
+
+alpha.manify(function(err, name){
+    if (err) throw err;
+    console.log('Twoje nowe imię to: ' + name);
+});
+
+alpha.save(function(err) {
+    if (err) throw err;
+
+    console.log('User ' + alpha.name + ' zapisany pomyślnie');
+});
+
+const mark = new User({
+    name: 'Mark',
+    username: 'Mark_pain_gain',
+    password: 'password'
+});
+
+mark.manify(function(err, name) {
+    if (err) throw err;
+    console.log('Twoje nowe imię to: ' + name);
+});
+
+mark.save(function(err) {
+    if (err) throw err;
+
+    console.log('Uzytkownik ' + mark.name +  ' zapisany pomyslnie');
+});
