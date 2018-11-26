5ffd2150e47017cda56b2bb7ea7ebbcdfc6f2860
Kamil Szczypior <kszczypior1@gmail.com>
2018-11-26 08:18:01 +0100

    Create promises methods

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const mongoose = require ('mongoose');
 const Schema = mongoose.Schema; // Pobieram główny konstruktor modelu mongoose
 
 mongoose.Promise = global.Promise;
-
-
 mongoose.connect('mongodb://localhost/nodeappdatabase', {
     useNewUrlParser: true
-});
- 
+}); 
 
-//Tworzę schemat dla aplikacji
+//Tworzę schemat dla aplikacji (new user Schema)
 const userSchema = new Schema({
     name: String,
     username: { type: String, required : true, unique: true},
@@ -19,7 +16,7 @@ const userSchema = new Schema({
     updated_at:Date
 });
 
-//Tworzę metody dla modelu Mongoose
+//Tmetody dla schema Mongoose
 userSchema.methods.manify = function(next) {
     this.name = this.name + '-men';
 
@@ -42,63 +39,9 @@ userSchema.pre('save', function(next){
     next();
 })
 
-//Tworzę model na podstawie schematu
+//Tworzę model na podstawie schematuuserSchema (model based on userSchema)
 const User = mongoose.model('User', userSchema);
 
-//Operacje CRUD z Mongoose. Znalezienie wszystkich rekordów w bazie
-User.find({}, function(err, res){
-    if (err) throw err;
-    console.log('Actual database records are' + res)
-})
-// to samo co wyzej tylko ES6 Promise
-const query = User.find({});
-const promise = query.exec();
-promise.then(function(records){
-    console.log('Actual database records are ' + records);
-});
-promise.catch(function(reason) {
-    console.log('Something went wrong:', reason);
-});
-
-//Znaleziesnie wybranych rekordów w bazie. (pasujących do zapytania)
-User.find({ username: 'Alpha_wolf'}).exec(function(err, res){
-    if (err) throw err;
-    console.log('Record you are looking for is ' + res);
-});
-
-//Aktualizowanie dokumentów. Tutaj hasła.
-User.find({ username: 'Alpha_wolf'}, function(err, user){
-    if (err) throw err;
-    console.log('Old password is ' + user[0].password);
-    user[0].password = 'newPassword';
-    console.log('New password is ' + user[0].password);
-
-    user[0].save(function(err) {
-        if (err) throw err;
-        console.log('Użytkownik ' + user[0].name + ' został pomyślnie zaktualizowany');
-    })
-});
-
-//Usuwanie dokumentów
-User.find({ username: 'Mark_pain_gain'}, function(err, user) {
-    if (err) throw err;
-    user = user[0];
-    user.remove(function(err) {
-        if (err) throw err;
-
-        console.log('User successfully deleted');
-    });
-});
-/* Wersja skrócona za pomocą wbudowanej metody która robi to samo (del user)
-User.findOneAndRemove({ username: 'Benny_the_man' }, function(err) {
-    if (err) throw err;
-
-    console.log('User deleted!');
-});
- */
-
-
-/*
 // Create instances of user object
 const kamil = new User({
     name: 'Kamil',
@@ -112,11 +55,6 @@ kamil.manify(function(err, name){
     console.log('Twoje nowe imię to: ' + name);
 });
 
-kamil.save(function(err){
-    if (err) throw err;
-
-    console.log('User zapisany pomyślnie');
-});
 //Tworzę więcej userów
 const alpha = new User({
     name: 'Alpha',
@@ -127,12 +65,6 @@ const alpha = new User({
 alpha.manify(function(err, name){
     if (err) throw err;
     console.log('Twoje nowe imię to: ' + name);
-});
-
-alpha.save(function(err) {
-    if (err) throw err;
-
-    console.log('User ' + alpha.name + ' zapisany pomyślnie');
 });
 
 const mark = new User({
@@ -146,9 +78,92 @@ mark.manify(function(err, name) {
     console.log('Twoje nowe imię to: ' + name);
 });
 
-mark.save(function(err) {
-    if (err) throw err;
+//Operacje CRUD z Mongoose.
+const findAllUsers = function() {
+    return User.find({}, function(err, res){
+        if (err) throw err;
+        console.log('Actual database records are' + res)
+    });
+}
 
-    console.log('Uzytkownik ' + mark.name +  ' zapisany pomyslnie');
-});
+const findSpecificRecord = function() {
+    //Znaleziesnie wybranych rekordów w bazie. (pasujących do zapytania)
+    return User.find({ username: 'Alpha_wolf'}, function(err, res){
+        if (err) throw err;
+        console.log('Record you are looking for is ' + res);
+    });    
+}
+
+const updateUserPassword = function() {
+    //zmiana hasła wybranego usera
+    return User.findOne({ username: 'Alpha_wolf'})
+        .then(function(user) {
+            console.log('Old password is ' + user.password);
+            console.log('Name ' + user.name);
+            user.password = 'newPassword';
+            console.log('New password is' + user.password);
+            return user.save(function(err) {
+                if (err) throw err;
+
+                console.log('Użytkownik ' + user.name + ' został poprawnie zaktualizowany');
+            })
+        })
+}
+
+const updateUsername = function() {
+    // zmiana nazwy użytkownika
+    return User.findOneAndUpdate({ username: 'Mark_pain_gain' }, { username: 'Mark_pain_gain' }, { new: true }, function(err, user) {
+        if (err) throw err;
+
+        console.log('Nazwa uzytkownika po aktualizacji to ' + user.username);
+    })
+}
+
+const findMarkAndDelete = function() {
+    // Znalezienie specyficznego usera i jego usunięcie
+    return User.findOne({ username: 'Mark_pain_gain' })
+        .then(function(user) {
+            return user.remove(function() {
+                console.log('User successfully deleted');
+            });
+        })
+}
+
+const findKennyAndDelete = function() {
+    // Znalezienie specyficznego usera i jego usunięcie
+    return User.findOne({ username: 'coincidence' })
+        .then(function(user) {
+            return user.remove(function() {
+                console.log('User successfully deleted');
+            });
+        });
+}
+
+const findBennyAndRemove = function() {
+    //Znalezienie specyficznego usera i jego usunięciee
+    return User.findOneAndRemove({ username: 'Alpha_wolf' })
+        .then(function(user) {
+            return user.remove(function() {
+                console.log('User successfully deleted');
+            });
+        });
+}
+/*
+findMarkAndDelete();
+findKennyAndDelete();
+findBennyAndRemove();
 */
+
+
+Promise.all([kamil.save(), alpha.save(), mark.save()])
+    .then(findAllUsers)
+    .then(findSpecificRecord)
+    .then(updateUserPassword)
+    .then(updateUsername)
+    .then(findMarkAndDelete)
+    .then(findKennyAndDelete)
+    .then(findBennyAndRemove)
+    .catch(console.log.bind(console))
+
+
+ 
